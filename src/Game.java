0721431e845d2ef74a0af5258eb3f9a54791472f
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-04-09 22:19:34 -0700

    class Tradcutor

---
+++ src/Game.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Move> moves;
+
+
+    // This methods takes a list of moves and returns a String with moves
+    public String listToGame(ArrayList<String> moves ){
+        String game = "";
+        //code
+
+        return game;
+    }
+
+    // This method takes String with moves and returns a game file in .pgn or .txt
+    public void stringToFile(String moves){
+        //code
+
+    }
+}
--- src/MaquinaTraductora.java
+++ src/MaquinaTraductora.java
@@ -1,20 +1,27 @@
+import java.util.ArrayList;
+
 public class MaquinaTraductora {
 
     public MaquinaTraductora() {
 
     }
-
+    // This method accepts a move in string format with descriptive notation and returns it
+    // into algebraic notation
     public String descripToAlgebTransMove(String move){
         String movAlg = "";
 
         return movAlg;
     }
-
-    public String descripToAlgebTransGame(String game){
+    // This method takes List of moves in descriptive notation and returns a list
+    // of moves in algebraic notation
+    public String descripToAlgebTransGame(ArrayList<String> game){
         String movAlg = "";
 
         return movAlg;
     }
+
+
+
 //   public String algebToDescripTrans(String mov){
 //        String movDescrip = "";
 //
