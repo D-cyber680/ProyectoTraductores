326bc25a49988a166b33bd2beb954c27067b7ba1
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-04-19 16:27:25 -0700

    tipos de juagadas

--- src/Move.java
+++ src/Move.java
@@ -6,10 +6,7 @@ public class Move {
     String nAlgebr;
     int tjugada; // 0: Non capture, 1: Capture, 2: Castling, 3: Promotion, 4: Invalid
     Boolean isWhite; //False: Black, True: White
-    Pattern patt1 = Pattern.compile("^[P|K|Q|R|B][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$");
-    Pattern patt2 = Pattern.compile("^[P|K|Q|R|B][x][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$");
-    Pattern patt3 = Pattern.compile("^O-O$|^O-O-O$");
-    Pattern patt4 = Pattern.compile("^P-(R|K(?!K|Q)|B|N|Q(?!K|Q))8/(Q|B|R|N)$");
+
     //"^[P|K|Q|R|B][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$" -> 0
     //"^[P|K|Q|R|B][x][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$" -> 1
     // "^O-O$|^O-O-O$" -> 2
