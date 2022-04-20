281dbc8593235d51efeca057fc0abe00bf0fd3d2
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-04-19 19:23:02 -0700

    tipos de juagadas

--- src/Move.java
+++ src/Move.java
@@ -13,23 +13,26 @@ public class Move {
     // "^P-(R|K(?!K|Q)|B|N|Q(?!K|Q))8/(Q|B|R|N)$" -> 3
 
 
-    public Move(String nDescrip, Boolean isWhite, String patt)
+    public Move(String nDescrip,String nAlgebr ,Boolean isWhite, String patt)
     {
-        this.nDescrip = nDescrip;
-        this.nAlgebr = null;
-        this.isWhite = isWhite;
 
-        if(Pattern.compile(nDescrip).matcher("^[P|K|Q|R|B][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$").matches()){
-             this.tjugada = 0;
-        }else if(Pattern.compile(nDescrip).matcher("^[P|K|Q|R|B][x][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$").matches() ){
-             this.tjugada = 1;
-        }else if(Pattern.compile(nDescrip).matcher("^O-O$|^O-O-O$" ).matches()){
-             this.tjugada = 2;
-        }else if(Pattern.compile(nDescrip).matcher("^P-(R|K(?!K|Q)|B|N|Q(?!K|Q))8/(Q|B|R|N)$").matches()){
-             this.tjugada = 3;
-        }else{
+        this.nDescrip = nDescrip;
+        this.nAlgebr = nAlgebr;
+        this.isWhite = isWhite;
+    //if(nAlgebr == null && nDescrip != null) {
+        if (Pattern.compile(nDescrip).matcher("^[P|K|Q|R|B][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$").matches()) {
+            this.tjugada = 0;
+        } else if (Pattern.compile(nDescrip).matcher("^[P|K|Q|R|B][x][1-8][RQ|NQ|BQ|Q|K|BK|NK|RK]$").matches()) {
+            this.tjugada = 1;
+        } else if (Pattern.compile(nDescrip).matcher("^O-O$|^O-O-O$").matches()) {
+            this.tjugada = 2;
+        } else if (Pattern.compile(nDescrip).matcher("^P-(R|K(?!K|Q)|B|N|Q(?!K|Q))8/(Q|B|R|N)$").matches()) {
+            this.tjugada = 3;
+        } else {
             this.tjugada = 4;
         }
+    //}
+
     }
     // Hacer uso de Maquina traductora para convertirlo a nAlgebraica
     //Si un movimiento tirar una Excepcion y decir donde esta el error
