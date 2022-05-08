d99ea768ad8684e21e28ecb84bbaa06fdcb112da
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-05-07 22:33:30 -0700

    falta implementar la funcion para la transformacion de columna y nousar tanto codigo

--- src/MaquinaTraductora.java
+++ src/MaquinaTraductora.java
@@ -5,67 +5,22 @@ public class MaquinaTraductora {
     public MaquinaTraductora() {
 
     }
+
     // This method accepts a move in string format with descriptive notation and returns it
     // into algebraic notation
-    public String descripToAlgebTransMove(Move move){
+    public String descripToAlgebTransMove(Move move) {
         String movAlg = move.nAlgebr;
         String piece, col, row;
-       // char[] columns = {'a', 'b', 'c', 'd', 'e','f','g','h'};
-        switch(move.tjugada)
-        {
+        // char[] columns = {'a', 'b', 'c', 'd', 'e','f','g','h'};
+        switch (move.tjugada) {
             case 0:
-                if (Character.toString(move.nDescrip.charAt(0)).equals("P"))
-                {
+                if (Character.toString(move.nDescrip.charAt(0)).equals("P")) {
                     piece = "";
-                }else {
+                } else {
                     piece = Character.toString(move.nDescrip.charAt(0)).toLowerCase();
                 }
-            row = Character.toString(move.nDescrip.charAt(1));
-            switch( move.nDescrip.substring(2,4) )
-            {
-                case "RQ":
-                    col = "a";
-                    break;
-                case "NQ":
-                    col = "b";
-                    break;
-                case "BQ":
-                    col = "c";
-                    break;
-                case "BK":
-                    col = "f";
-                    break;
-                case "NK":
-                    col = "g";
-                    break;
-                case "RK":
-                    col = "h";
-                    break;
-                default:
-                    switch (move.nDescrip.substring(2,3))
-                    {
-                        case "K":
-                            col = "e";
-                            break;
-                        case "Q":
-                            col = "d";
-                            break;
-                        default:
-                            col = "";
-                    }
-                    move.nAlgebr = piece + col + row;
-            }
-                break;
-            case 1:
-                if ( Character.toString(move.nDescrip.charAt(0)).equals("P") )
-                {
-                    piece = "";
-                }else {
-                    piece = Character.toString(move.nDescrip.charAt(0)).toLowerCase();
-                }
-                row = Character.toString(move.nDescrip.charAt(2));
-                switch( move.nDescrip.substring(3,5) )
-                {
+                row = Character.toString(move.nDescrip.charAt(1));
+                switch (move.nDescrip.substring(2, 4)) {
                     case "RQ":
                         col = "a";
                         break;
@@ -85,8 +40,47 @@ public class MaquinaTraductora {
                         col = "h";
                         break;
                     default:
-                        switch (move.nDescrip.substring(3, 4))
-                        {
+                        switch (move.nDescrip.substring(2, 3)) {
+                            case "K":
+                                col = "e";
+                                break;
+                            case "Q":
+                                col = "d";
+                                break;
+                            default:
+                                col = "";
+                        }
+                        move.nAlgebr = piece + col + row;
+                }
+                break;
+            case 1:
+                if (Character.toString(move.nDescrip.charAt(0)).equals("P")) {
+                    piece = "";
+                } else {
+                    piece = Character.toString(move.nDescrip.charAt(0)).toLowerCase();
+                }
+                row = Character.toString(move.nDescrip.charAt(2));
+                switch (move.nDescrip.substring(3, 5)) {
+                    case "RQ":
+                        col = "a";
+                        break;
+                    case "NQ":
+                        col = "b";
+                        break;
+                    case "BQ":
+                        col = "c";
+                        break;
+                    case "BK":
+                        col = "f";
+                        break;
+                    case "NK":
+                        col = "g";
+                        break;
+                    case "RK":
+                        col = "h";
+                        break;
+                    default:
+                        switch (move.nDescrip.substring(3, 4)) {
                             case "K":
                                 col = "e";
                                 break;
@@ -103,15 +97,29 @@ public class MaquinaTraductora {
                 break;
             case 3:
                 piece = "";
-                switch (move.nDescrip.length()){
+                switch (move.nDescrip.length()) {
                     case 6:
-                        if (Character.toString(move.nDescrip.charAt(2)).equals("K")){
+                        if (Character.toString(move.nDescrip.charAt(2)).equals("K")) {
                             col = "e";
-                        }else if(Character.toString(move.nDescrip.charAt(2)).equals("Q")){
+                        } else {
                             col = "d";
                         }
                         break;
                     case 7:
+                        switch (move.nDescrip.substring(2, 4)) {
+                            case "RK":
+                                break;
+                            case "NK":
+                                break;
+                            case "BK":
+                                break;
+                            case "RQ":
+                                break;
+                            case "NQ":
+                                break;
+                            case "BQ":
+                                break;
+                        }
                         break;
                 }
                 break;
@@ -121,14 +129,14 @@ public class MaquinaTraductora {
 
         return movAlg;
     }
+
     // This method takes List of moves in descriptive notation and returns a list
     // of moves in algebraic notation
-    public String descripToAlgebTransGame(ArrayList<String> game){
+    public String descripToAlgebTransGame(ArrayList<String> game) {
         String movAlg = "";
 
         return movAlg;
     }
-
 
 
 //   public String algebToDescripTrans(String mov){
@@ -136,5 +144,11 @@ public class MaquinaTraductora {
 //
 //        return movDescrip;
 //    }
+
+    public static String colDescriptoAlg(Move move){
+        //returns the letter of the column in algebraic notation according yo
+        // the --type of move-- using substring method
+        return "";
+    }
 
 }
