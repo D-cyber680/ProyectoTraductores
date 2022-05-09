8c8d384498709813961f5f7033ed47b0dd189a94
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-05-08 19:44:42 -0700

    StringGametolistMoves

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Game {
     ArrayList<Move> moves;
-    public ArrayList<Move> StringGametolistMoves(String game){
+
+    public Game(String game) {
+        StringGametolistMoves(game);
+    }
+
+    public void StringGametolistMoves(String game){
         String[] gamename = game.split("\\s+");
         boolean iswhite = true;
-        ArrayList<Move> moves = null;
         for (int i = 0; i < gamename.length; i++)
         {
             if(i % 3 != 0){
-                moves.add(new Move(gamename[i],null,iswhite));
+                this.moves.add(new Move(gamename[i],null,iswhite));
                 iswhite = !iswhite;
             }
         }
-        return moves;
     }
 
     // This methods takes a list of moves and returns a String with moves
--- src/Traductor.java
+++ src/Traductor.java
@@ -3,9 +3,9 @@ import java.util.regex.Pattern;
 public class Traductor {
     public static void main(String[] args) {
 
-        Move movimiento = new Move("P-K8/B",null,false);
-        MaquinaTraductora trad = new MaquinaTraductora();
-        trad.descripToAlgebTransMove(movimiento);
-        System.out.println(movimiento.nAlgebr);
+//        Move movimiento = new Move("P-K8/B",null,false);
+//        MaquinaTraductora trad = new MaquinaTraductora();
+//        trad.descripToAlgebTransMove(movimiento);
+//        System.out.println(movimiento.nAlgebr);
     }
 }
