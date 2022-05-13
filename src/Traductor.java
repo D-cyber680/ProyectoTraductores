ad8ac350b51666fea639251a34e2ba4015a4f880
D-cyber680 <72686197+D-cyber680@users.noreply.github.com>
2022-05-12 20:30:50 -0700

    Logica terminada (parte 1) sigue GUI, importar chess engine

--- src/Traductor.java
+++ src/Traductor.java
@@ -3,8 +3,13 @@ import java.util.regex.Pattern;
 public class Traductor {
     public static void main(String[] args) {
 
-        Move movimiento = new Move("B4RQ",null,false);
-        MaquinaTraductora.descripToAlgebTransMove(movimiento);
-        System.out.println(movimiento.nAlgebr);
+        String game_desc = "1. P4K P4K 2. N3BK B3BQ 3. B4BQ B4BQ 4. P4NQ Bx4NQ 5. P3BQ B4BQ 6. P4Q Px4Q 7. O-O P6Q ";
+        Game new_game = new Game(game_desc);
+        System.out.println(new_game.moves.toString());
+        System.out.println(new_game.listToGameAlg());
+
+//        Move movimiento = new Move("P6Q",null,false);
+//        MaquinaTraductora.descripToAlgebTransMove(movimiento);
+//        System.out.println(movimiento.nAlgebr);
     }
 }
